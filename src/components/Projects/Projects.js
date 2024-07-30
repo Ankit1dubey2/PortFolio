20f7e952750acbc6b5871efc503b6bd1bb610d43
Ankit1dubey2 <ankitdubeys007@gmail.com>
2024-07-30 15:00:05 +0530

    Portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,6 +25,7 @@ function Projects() {
               title="News Mania"
               description="A news aggregation application built with the MERN stack and NewsAPI. It collects and displays real-time news from multiple sources, featuring a server component for data management and a client interface for a seamless user experience. Users can view, filter, and explore articles by category, ensuring they stay updated with the latest news."
               ghLink="https://github.com/Ankit1dubey2/News-Mania"
+              demoLink="https://news-mania-sigma.vercel.app/"
             />
           </Col>
 
@@ -35,6 +36,7 @@ function Projects() {
               title="Swaad Ghar Tak"
               description="A food ordering platform developed with NodeJS, Express, MongoDB, Mongoose, and EJS. It enables users to effortlessly place orders from their favorite restaurants, featuring dynamic menu management and real-time updates. The platform offers a responsive and intuitive interface, ensuring a smooth and efficient online ordering experience."
               ghLink="https://github.com/Ankit1dubey2/Swaad-Ghar-Tak"
+              demoLink="https://swaad-ghar-tak.onrender.com/"
             />
           </Col>
 
@@ -44,7 +46,8 @@ function Projects() {
               isBlog={false}
               title="Blood Bank"
               description="A web application for managing blood inventory, donors, hospitals, and organizations, built with Express, Node.js, MongoDB, and React. It features role-specific dashboards for Admin, Organization, Donor, and Hospital, with real-time record-keeping and contribution tracking. The tech stack includes React, Redux, Tailwind CSS, and MongoDB."
-              ghLink="https://github.com/Ankit1dubey2/Blood-Bank"              
+              ghLink="https://github.com/Ankit1dubey2/Blood-Bank"  
+              demoLink="https://blood-bank-five-pied.vercel.app/"            
             />
           </Col>
         </Row>
